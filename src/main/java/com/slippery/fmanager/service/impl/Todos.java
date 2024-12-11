9f9ad408b9a36c67e1b13e0194a306d4c8e9eee0
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-12-11 17:23:37 +0300

    update what to cover today

--- src/main/java/com/slippery/fmanager/service/impl/Todos.java
+++ src/main/java/com/slippery/fmanager/service/impl/Todos.java
@@ -1,13 +1,13 @@
 public class Todos {
     /*
-    *TODO: USER login and jwt auth
-    * jwt token creation and issuing
-    * wallet creation
-    * savings account
-    * transactions history
-    * transactions
-    * incomes
-    * expenditure
+    *TODO: USER login and jwt auth -done
+    * jwt token creation and issuing -done
+    * wallet creation -done
+    * savings account -pending(today)
+    * transactions history -done
+    * transactions -done
+    * incomes -not done
+    * expenditure -not done
     * */
 
 }
